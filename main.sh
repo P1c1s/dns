1144547fa68b114c8f81f54bd1ca58b8d3fbb2e4
P1c1s <56824204+P1c1s@users.noreply.github.com>
2019-10-30 18:12:05 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,20 +3,53 @@
 #|-------------------|
 #|  NAME OF SCIRPT   |
 #|-------------------|
-
 clear
+for ((i=0;i<10;i++ ))do
+    n1=$[$i/2]
+    n2=$[$n1*2]
+    test $i -ne $n2 && flag=0 || flag=1
+    clear
+    if [ "$flag" -eq "0" ]; then
+    echo
+    echo
+    echo -e '\033[0;31m                   (                                  )'
+    echo -e '                  )\ ) (  (     (  (  (       (   ( /('
+    echo -e '                 (()/( )\ )(   ))\ )\))(   (  )(  )\())('
+    echo -e '                 /(_)((_(()\ /((_((_)()\  )\(()\((_)\ )\'
+    echo -e '                (_)\033[0;34m _|\033[0;31m(_)((_(_)) _(()((_)((_)((_| |(_((_)'
+    echo -e '\033[0;34m                 |  _|| |  _/ -_)\ V  V / _ |  _| / /(_-<'
+    echo -e '                 |_|  |_|_| \___| \_/\_/\___|_| |_\_\/__/'
+    else
+    echo
+    echo
+    echo -e '\033[0;31m                   (                                )'
+    echo -e '                 )\ ) (  (     (  (  (       (   ( /('
+    echo -e '                 (()/( )\ )(   ))\ )\))(   (  )(  )\())('
+    echo -e '                 /(_)((_(()\ /((_((_)()\  )\(()\((_)\ )\'
+    echo -e '               (_)\033[0;34m _|\033[0;31m(_)((_(_)) _(()((_)((_)((_| |(_((_)'
+    echo -e '\033[0;34m                 |  _|| |  _/ -_)\ V  V / _ |  _| / /(_-<'
+    echo -e '                 |_|  |_|_| \___| \_/\_/\___|_| |_\_\/__/'
+    fi
+    sleep 0.2s
+done
 echo
 echo
-echo -e '\033[0;31m                   (                                  )'
-echo -e '                  )\ ) (  (     (  (  (       (   ( /('
-echo -e '                 (()/( )\ )(   ))\ )\))(   (  )(  )\())('
-echo -e '                 /(_)((_(()\ /((_((_)()\  )\(()\((_)\ )\'
-echo -e '                (_)\033[0;34m _|\033[0;31m(_)((_(_)) _(()((_)((_)((_| |(_((_)'
-echo -e '\033[0;34m                 |  _|| |  _/ -_)\ V  V / _ |  _| / /(_-<'
-echo -e '                 |_|  |_|_| \___| \_/\_/\___|_| |_\_\/__/'
 echo
-echo
-echo
+
+#clear
+#echo
+#echo
+#echo -e '\033[0;31m                   (                                  )'
+#echo -e '                  )\ ) (  (     (  (  (       (   ( /('
+#echo -e '                 (()/( )\ )(   ))\ )\))(   (  )(  )\())('
+#echo -e '                 /(_)((_(()\ /((_((_)()\  )\(()\((_)\ )\'
+#echo -e '                (_)\033[0;34m _|\033[0;31m(_)((_(_)) _(()((_)((_)((_| |(_((_)'
+#echo -e '\033[0;34m                 |  _|| |  _/ -_)\ V  V / _ |  _| / /(_-<'
+#echo -e '                 |_|  |_|_| \___| \_/\_/\___|_| |_\_\/__/'
+#echo
+#echo
+#echo
+
 
 #|-------------------|
 #|     Print Menù    |
@@ -29,15 +62,19 @@ echo -e '\033[0;31m|\033[0;34m 0) Exit                                          
 sleep 0.5s
 echo -e '\033[0;31m|\033[0;34m 1) Manual                                                        \033[0;31m|'
 sleep 0.5s
-echo -e '\033[0;31m|\033[0;34m 2) Add new sites                                                 \033[0;31m|'
+echo -e '\033[0;31m|\033[0;34m 2) Add new sites to the list                                     \033[0;31m|'
 sleep 0.5s
-echo -e '\033[0;31m|\033[0;34m 3) Run                                                           \033[0;31m|'
+echo -e '\033[0;31m|\033[0;34m 3) Update list                                                   \033[0;31m|'
 sleep 0.5s
-echo -e '\033[0;31m|\033[0;34m 4) Empty iptables                                                \033[0;31m|'
+echo -e '\033[0;31m|\033[0;34m 4) Add rules to iptables                                         \033[0;31m|'
+sleep 0.5s
+echo -e '\033[0;31m|\033[0;34m 5) Empty iptables                                                \033[0;31m|'
+sleep 0.5s
+echo -e '\033[0;31m|\033[0;34m 6) Generate script for crontab                                   \033[0;31m|'
 sleep 0.5s
 echo -e '\033[0;34m+\033[0;31m------------------------[\033[0;34mfireworks 2019\033[0;31m]--------------------------\033[0;34m+'
 sleep 0.8s
-read -p "Valore atteso: " input
+read -p "Expected value: " input
 
 #EXIT
 if [[ "$input" -eq 0 ]] ; then
@@ -49,7 +86,16 @@ if [ "$input" -eq "1" ]; then
     cat manual.txt
 fi
 
-#EXECUTION
+#MODIFY LIST
+if [ "$input" -eq "2" ]; then
+    read -p "How many sites do you want add? " sites
+    for (( i=0; i<sites;    )) do
+    read -p "Add $i site: " site
+    sudo iptables -A OUTPUT -d $site -j DROP
+    done
+fi
+
+#Update
 if [ "$input" -eq "3" ]; then
 
 #|-------------------|
@@ -95,6 +141,11 @@ host -t a twitter.com >> $file
 host -t a twitter.fr >> $file
 host -t a twitter.it >> $file
 
+echo -e "The blacklist has been updated, check $file"
+fi
+
+#Add rules
+if [ "$input" -eq "4" ]; then
 
 #|-------------------|
 #|    Extract ip     |
@@ -121,7 +172,7 @@ done
 fi
 
 #EMPTY IPTABLES
-if [ "$input" -eq "4" ]; then
+if [ "$input" -eq "5" ]; then
     #Line includes 2 rows of text
     line=sudo iptables --line-numbers -L OUTPUT | wc -l
     #Only rules
@@ -130,4 +181,23 @@ if [ "$input" -eq "4" ]; then
     do
        sudo iptables -D OUTPUT 1
     done
+fi
+
+
+#EMPTY IPTABLES
+if [ "$input" -eq "6" ]; then
+    path=fireworks.sh
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
+    echo >> $path
 fi
